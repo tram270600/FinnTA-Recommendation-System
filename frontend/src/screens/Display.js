fc7d38d121929757c53f7bcfc78296a2f1f659b5
bannybontieu <bannybontieu@gmail.com>
2021-12-09 22:06:54 +0700

    Change file location

---
+++ frontend/src/screens/Display.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import 'w3-css/w3.css';
+import '../styles/Display.scss';
+import data from '../data/products';
+
+const Display = () => {
+    return (
+        <>
+            {/* Menu */}
+            <div className="Menu">
+                <ul className='NavMenu dash'>
+                    <li className='MenuItems'>
+                        <a href="#" className='MenuLinks' id="other">
+                            Saved
+                        </a>
+                    </li>
+                    <li className='MenuItems'>
+                        <a href="#" className='MenuLinks' id="current">
+                            Products
+                        </a>
+                    </li>
+                    <li className='MenuItems'>
+                        <a href='#' className='MenuLinks' id="other">
+                            Styles
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            {/* Display */}
+            <div className="DisplayScreen">
+                {data.products.map((productDisplay) => (
+                    <div className="item">
+                        <img src={productDisplay.product_image} alt="Norway" className="picture" />
+                        <div className="detail">
+                            <div className = "info">
+                                <p className="name">
+                                    {productDisplay.product_name}
+                                </p>
+                                <p className="description">
+                                    {productDisplay.product_description}
+                                </p>
+                                <p className="price">
+                                    {productDisplay.product_price}
+                                </p>
+                            </div>
+
+                            <div className ="button">
+                                <a href ={"/editpost/" + productDisplay.product_id} className="edit">Edit</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
+
+export default Display;
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import SideBar from './SideBar';
+import NavBar from '../components/NavBar';
+import Display from './Display';
+import '../styles/homepage.css';
 
-export default function ProfileScreen(){
-  return (
-    <div> ProfileScreen </div>
-  );
-  
+const ProfileScreen = ({ Items }) => {
+    return (
+        <>
+            <div className="Page">
+                <NavBar />
+                <div className="SideBar">
+                    <SideBar productItem={Items} />
+                </div>
+                <div className="PageDisplay">
+                    <Display productItem={Items} />
+                </div>
+            </div>
+        </>
+    );
 }
+
+export default ProfileScreen;

e9db05cd943ef7b8377d759ee19baaec34c65e01
bannybontieu <bannybontieu@gmail.com>
2021-12-18 16:43:51 +0700

    Revert "- rotate, control 3D object, and example of shoes"
    This reverts commit b879ae8f2502f75b68f648e5012731cb913f2250.

--- frontend/src/screens/ClothesObject.js
+++ frontend/src/screens/ClothesObject.js
@@ -1,11 +1,9 @@
-import React, { Suspense, useRef, useState } from 'react'
+import React, { Suspense, useRef } from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 
-import {Html, useGLTF, OrbitControls, ContactShadows} from '@react-three/drei'
-// import {useSpring, a} from "react-spring/three"
-import { useSpring, animated } from '@react-spring/three'
+import {Html, useGLTF} from '@react-three/drei'
 
 const Model = ({modelPath}) => {
   const gltf = useGLTF(modelPath, true)
@@ -14,23 +12,13 @@ const Model = ({modelPath}) => {
 
 const HTMLContent = ({modelPath}) => {
   // const ref = useRef();
-  // useFrame(() => (ref.current.rotation.y += 0.01))
-  const [expand, setExpand] = useState(false);
-  const props = useSpring({
-    scale: expand ? [1.4, 1.4, 1.4] : [1, 1, 1],
-
-  })
+  // useFrame(()=> (ref.current.rotation.x += 0.01));
   return (
-    <group>
-      <animated.mesh 
-      onClick={() => setExpand(!expand)}
-      scale={props.scale}
-      // ref={ref}
-      position={[0,0,0]}>
-      <Model modelPath={modelPath} />
-      </animated.mesh>
-    </group>
-
+    <mesh 
+    // ref={ref} 
+    position={[0,35,0]}>
+    <Model modelPath={modelPath} />
+    </mesh>
 
   )
 }
@@ -51,22 +39,15 @@ export default function ClothesObject(){
   return (
     <>
     <NavBar />
-    <Canvas style={{ height: '100vh'}}
+    <Canvas
       colorManagement
-      camera={{position: [400, -100, 500], fov: 20}}>
+      camera={{position: [0, 0, 120], fov: 70}}>
       <Lights/>
       <Suspense fallback={null}>
        <HTMLContent modelPath = 'floralDress.gltf'/>
-       <ContactShadows rotation-x={Math.PI/2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={2} far={1}/>
       </Suspense>
-      <OrbitControls
-        enableZoom={true}
-        enablePan={true}
-        enableRotate={true}
-        zoomSpeed={0.6}
-        panSpeed={0.5}
-        rotateSpeed={0.4}/>
     </Canvas>
+    <Footer />
     </>
   );
   

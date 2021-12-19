ab3cd46026b55e31839734669fba4352b58f16f2
Duccuteee <64661534+Duccutee@users.noreply.github.com>
2021-12-19 14:15:04 +0700

    login register data server nha, kh hiểu t có ghi tham khảo á
    server bỏ ngoài, server fake api với get token cho từng cái lun
    mở 1 terminal riêng cho server , npm run auth để server chạy
    nếu có lỗi hoặc login thành công, nó sẽ trả về err 404 401... hoặc 200 OK
    
    post get thì làm bth thui, mà hình như cái của t gọi sai hàm post của register ở server.js hay  regis.js hay sao á ._. thức đêm làm cúp điện mấy tiếng cái lú lun

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,42 +1,83 @@
 import React, {useState} from 'react'
 import '../styles/Login.css'
+import {useHistory, Link} from 'react-router-dom'
+import axiox from "axios";
 import VisibilityIcon from '@mui/icons-material/Visibility'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
 function Login() {  
+const [username,setUsername]= useState("");
+const [password,setPassword]= useState("");
+const [showPass,setshowPass]= useState(false);
+const [error, setError]= useState("");  
+let history = useHistory();
 
-const [showPass,setshowPass]= useState(false);    
+const login = (e) => {
+  e.preventDefault();
+  axiox
+    .post("http://localhost:5001/api/auth/login", {
+      username,
+      password,
+    })
+    .then((response) => {
+      console.log("response", response);
+      localStorage.setItem(
+        "login",
+        JSON.stringify({
+          userLogin: true,
+          token: response.data.access_token,
+        })
+      );
+      setError("");
+      setUsername("");
+      setPassword("");
+
+      history.push("/");
+    })
+    .catch((error) => setError(error.response.data.message));
+};
     return (
         <div class="rside">
       <div class= "lside">
-       
+      
       </div>
         
-          <form method="post">
+          <form onSubmit={login}>
             <h1>Log in</h1>
             <p><p1>Sign in to</p1> your FinCon Account!!</p>
+               
               <div class="txtfield">
-                    <input type="text" required/>
+                    <input type="text" required 
+                    value={username} 
+                    onChange={(e)=> setUsername(e.target.value)}/>
                     <span></span>
-                    <label>Email adress</label>
+                    <label>Username</label>
+
               </div>
              
               <div class="txtfield1">
-                 <input type={showPass ?"text":"password" }required/>
+                 <input type={showPass ?"text":"password" } required  
+                 value={password}
+                 onChange={(e)=> setPassword(e.target.value)}/>
                     <span></span>
                  <label>Password</label>
                  </div>
                  <div class="passwordicon" onClick={()=>{if(showPass == true){setshowPass(false)}else {setshowPass(true)}}}> 
                  {showPass ? <VisibilityIcon/> : <VisibilityOffIcon/>}
               </div>
+              
               <div id="general1">
              <div class="signuplink">
-                Don't have an account? <a href="#">Signup</a>
+                Don't have an account? <Link to="/sign-up">Signup</Link>
             </div>
         <input type="submit" value="Login"/>
-        </div>
-
-              
-             
+        </div> 
+       
+        {error && <p2 style={{
+          color:"red",
+           position:"absolute",
+           top:"580px", width:"500px", left:"15vw"
+           }}>{error}</p2>}
+            
           </form>
      </div>
     )
